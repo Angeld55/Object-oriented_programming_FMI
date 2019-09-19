50755fd0286f1c1fd605611644a14848b311c050
Angeld55 <achi_d@abv.bg>
2019-09-19 19:22:59 +0300

    Create Drink.h

---
+++ Big4AndInheritance/Drink.h
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Drink
+{
+private:
+	char* mark;
+	int ml;
+	void CopyFrom(const Drink& other);
+	void Free();
+public:
+	Drink();
+	Drink(const char* mark, int ml);
+	Drink(const Drink& other);
+	Drink& operator=(const Drink& other);
+	~Drink();
+	
+	void setML(int ml);
+	int getML() const;
+	const char* getMark() const;
+};
