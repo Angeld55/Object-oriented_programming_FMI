97635dc430c989791c3e17fa8a633e87033d29b6
Angeld55 <achi_d@abv.bg>
2019-09-20 15:00:26 +0300

    Add files via upload

--- Classes/GradesBook(DynamicArray)/GradesBook.h
+++ Classes/GradesBook(DynamicArray)/GradesBook.h
@@ -1,1 +1,24 @@
-d
+class GradesBook
+{
+	int* grades;
+	int size;
+	int capacity;
+	
+	bool isValidGrade(int grade);
+	void Resize();
+	void CopyFrom(const GradesBook& other);
+	void Free();
+public:
+	GradesBook();
+	GradesBook(const GradesBook& other);
+	GradesBook& operator=(const GradesBook& other);
+	~GradesBook();
+	
+	bool isFull();
+	void AddGrade(int grade);
+	void RemoveLastGrade();
+	
+	int operator[](int index) const;
+	friend ostream& operator<<(ostream& stream, GradesBook& obj);
+
+};
