b233a370168ea6239f8233a299460f8f55cbf3c7
Angel Dimitriev <achi_d@abv.bg>
2021-04-10 11:21:08 +0300

    Update Time.h

--- Sem. 06/Time/Time.h
+++ Sem. 06/Time/Time.h
@@ -12,9 +12,9 @@ public:
 	Time(size_t hours, size_t mins, size_t seconds);
  
 
-	size_t getHours();
-	size_t getMins();
-	size_t getSeconds();
+	size_t getHours() const;
+	size_t getMins() const;
+	size_t getSeconds() const;
 	void setHours(size_t hours);
 	void setMins(size_t mins);
 	void setSeconds(size_t seconds);
