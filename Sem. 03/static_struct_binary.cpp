2b90474d0ea888a7a074d0ee1fc0825008f135f0
Angel Dimitriev <angel_dimitriev@abv.bg>
2022-03-08 19:30:34 +0200

    Create static_struct_binary.cpp

---
+++ Sem. 03/static_struct_binary.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#pragma warning(disable:4996)
+using namespace std;
+
+
+struct Test
+{
+	bool b;
+	int x;
+};
+
+int main()
+{
+
+	{ //write
+		Test t = { true, 45 };
+		ofstream file("testObj.bat", ios::binary);
+
+		file.write((char*)&t, sizeof(t));
+	}
+
+	{ //read
+		Test t;
+		ifstream file("testObj.bat", ios::binary);
+
+		file.read((char*)&t, sizeof(t));
+		std::cout << t.b << " " << t.x << std::endl;
+	}
+
+}
