33320352d39d8881a0c92c189ca121f85e61036e
Angel Dimitriev <achi_d@abv.bg>
2022-03-15 19:27:04 +0200

    Add files via upload

---
+++ array_of_students_write.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+
+struct Student
+{
+	char name[30];
+	int age;
+	int fn;
+};
+
+void initStudent(Student& st, const char* name, int age, int fn)
+{
+	strcpy(st.name, name);
+	st.age = age;
+	st.fn = fn;
+}
+
+void saveToFile(const Student* students, size_t count, ofstream& file)
+{
+	file.write((const char*)students, count * sizeof(Student));
+}
+
+
+int main()
+{
+	Student* arr = new Student[4];
+	initStudent(arr[0], "ivan", 44, 1234);
+	initStudent(arr[1], "petur", 12, 765);
+	initStudent(arr[2], "alex", 15, 44);
+	initStudent(arr[3], "katerina", 19, 12134);
+
+	ofstream file("students.dat", ios::binary);
+	saveToFile(arr, 4, file);
+	
+	delete[] arr;
+}
