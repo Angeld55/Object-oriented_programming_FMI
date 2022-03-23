db3676a292f73216743d918955d4755e4da38a55
Angel Dimitriev <angel_dimitriev@abv.bg>
2022-03-23 03:16:37 +0200

    Update Student.cpp

--- Sem. 05/Student.cpp
+++ Sem. 05/Student.cpp
@@ -13,7 +13,7 @@ bool containsOnlySmallChars(const char* str)
 {
 	size_t len = strlen(str);
 
-	for (int i = 0; i < len; i++)
+	for (size_t i = 0; i < len; i++)
 	{
 		if (!isSmallLetter(str[i]))
 			return false;
