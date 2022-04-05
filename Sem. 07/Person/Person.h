af355cb13b5cf168000b8461e6dfd89650cb6a86
Angel Dimitriev <achi_d@abv.bg>
2022-04-05 20:09:04 +0300

    Add files via upload

--- Sem. 07/Person/Person.h
+++ Sem. 07/Person/Person.h
@@ -1,1 +1,27 @@
-d
+#pragma once
+#include <iostream>
+class Person
+{
+	char* name = nullptr;
+	int age;
+
+	void copyFrom(const Person& other);
+	void free();
+
+public:
+	Person() = default;
+	Person(const char* name, int age);
+
+	Person(const Person& other);
+	Person& operator=(const Person& other);
+
+	const char* getName() const;
+	int getAge() const;
+
+	void setName(const char* name);
+	void setAge(int age);
+
+	~Person();
+	void print() const;
+
+};
---
+++ Sem. 07/Person/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include "Person.h"
+using namespace std;
+
+int main()
+{
+	Person p("Ivan", 43);
+	p.print();
+
+}
