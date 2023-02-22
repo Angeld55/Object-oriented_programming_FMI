5cc97948a402c36098123baffef68ed320c203f9
Angel Dimitriev <angel_dimitriev@abv.bg>
2023-02-22 15:57:18 +0200

    Create enum_example_1.cpp

---
+++ 1. Lectures/1. Introduction to user defined data types/enum_example_1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+enum Color { red, green, blue };                    // plain enum 
+enum Card { red_card, green_card, yellow_card };    // another plain enum 
+enum class Animal { dog, deer, cat, bird, human };  // enum class
+enum class Mammal { kangaroo, deer, human };        // another enum class
+
+int main() 
+{
+
+    // examples of bad use of plain enums:
+    Color color = Color::red;
+    Card card = Card::green_card;
+
+    int num = color;    // no problem
+
+    if (color == Card::red_card) // no problem (bad)
+        cout << "bad" << endl;
+
+    if (card == Color::green)   // no problem (bad)
+        cout << "bad" << endl;
+
+    // examples of good use of enum classes (safe)
+    Animal a = Animal::deer;
+    Mammal m = Mammal::deer;
+
+    int num2 = a;   // error
+    if (m == a)         // error (good)
+        cout << "bad" << endl;
+
+    if (a == Mammal::deer) // error (good)
+        cout << "bad" << endl;
+
+}
