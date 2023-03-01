899360e045419ac157d08a1a1bb315f1fba5112d
Angel Dimitriev <angel_dimitriev@abv.bg>
2023-03-01 17:10:34 +0200

    Update ipAddress.cpp

--- Week 01/ipAddress.cpp
+++ Week 01/ipAddress.cpp
@@ -1,21 +1,11 @@
-
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*********************************************************************
 #include <iostream>
 #include <fstream>
-#include <assert.h>
-
 
 union IpAddress
 {
 	uint32_t address;
 	unsigned char octets[4];
 };
-
 
 int main()
 {
