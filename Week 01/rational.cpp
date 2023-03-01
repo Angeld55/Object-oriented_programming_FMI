344ecd6791221cf6043b2b7d7f7b0856a207d073
Angel Dimitriev <angel_dimitriev@abv.bg>
2023-03-01 02:21:12 +0200

    Update rational.cpp

--- Week 01/rational.cpp
+++ Week 01/rational.cpp
@@ -1,15 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-#include <iostream>
-
-using namespace std;
-
 #include <iostream>
 #include <fstream>
 #include <assert.h>
