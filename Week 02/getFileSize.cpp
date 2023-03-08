3d046371d7b53f6c7438f09c402c49db5ba1372f
Angel Dimitriev <angel_dimitriev@abv.bg>
2023-03-08 19:36:54 +0200

    Update and rename getFileSize_temp.cpp to getFileSize.cpp

---
+++ Week 02/getFileSize.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+
+size_t getFileSize(std::ifstream& file)
+{
+	size_t currentPos = file.tellg();
+	file.seekg(0, std::ios::end);
+	size_t fileSize = file.tellg();
+	file.seekg(currentPos);
+	return fileSize;
+}
+
+int main()
+{
+}
