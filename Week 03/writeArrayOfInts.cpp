448939e9d3be540eee7a2be7b80e4fb031dbae4b
Angel Dimitriev <angel_dimitriev@abv.bg>
2023-03-08 19:38:41 +0200

    Create writeArrayOfInts.cpp

---
+++ Week 03/writeArrayOfInts.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	std::ofstream ofs("myfile.dat", std::ios::out |
+									std::ios::binary);
+
+	int arr[5] = { 1,2,3,4,5 };
+
+	ofs.write((const char*)arr, 5 * sizeof(int));
+
+	ofs.close();
+}
