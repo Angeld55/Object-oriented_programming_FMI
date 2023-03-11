91bf68bc7e3a73cf5d35f4fb1ec02240f6966b05
Miroslav Stoyanovski <m.stoyanovski3@gmail.com>
2023-03-11 16:11:46 +0200

    Fixed endianness
    Shouldn't we print the octets backwards since we are assuming little-endian machine?

--- Week 01/ipAddress.cpp
+++ Week 01/ipAddress.cpp
@@ -12,10 +12,10 @@ int main()
 	IpAddress myAddress;
 	myAddress.address = 16777343;
 	
-	std::cout << (int)myAddress.octets[0] << "." 
-	          << (int)myAddress.octets[1] << "." 
+	std::cout << (int)myAddress.octets[3] << "." 
 	          << (int)myAddress.octets[2] << "." 
-	          << (int)myAddress.octets[3];
+	          << (int)myAddress.octets[1] << "." 
+	          << (int)myAddress.octets[0];
 	
 }
 
