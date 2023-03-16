8a8eb06f5bd9e491ffdd9c4ff19776a9cb0ccae4
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-03-16 19:59:10 +0200

    Create PrimeIterator.cpp

---
+++ Week 04/PrimeIterator.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+class PrimeIterator
+{
+	size_t _value;
+
+	bool isPrime(size_t n)
+	{
+		if (n < 2)
+			return false;
+		double sqrtN = sqrt(n);
+		for (unsigned i = 2; i <= sqrtN; i++)
+		{
+			if (n % i == 0)
+				return false;
+		}
+		return true;
+	}
+public:
+	PrimeIterator(size_t value)
+	{
+		setValue(value);
+	}
+	PrimeIterator() : PrimeIterator(2)
+	{}
+	
+	size_t getValue() const
+	{
+		return _value;
+	}
+	void setValue(size_t value)
+	{
+		if (isPrime(value))
+			_value = value;
+		else
+			_value = 2;
+	}
+
+	void goToNext()
+	{
+		//???
+		while (!isPrime(++_value)) {}
+	}
+	void goToPrev()
+	{
+		if (_value == 2)
+			return;
+		while (!isPrime(--_value)) {}
+	}
+
+};
+int main()
+{
+	
+	for (PrimeIterator i(2); i.getValue() < 100 ; i.goToNext())
+		std::cout << i.getValue() << std::endl;
+}
