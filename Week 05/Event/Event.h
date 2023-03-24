719b35ee8d073f42b4afd9c18343a4a4448a5983
Angel Dimitriev <angel_dimitriev@abv.bg>
2023-03-24 16:20:37 +0200

    Update Event.h

--- Week 05/Event/Event.h
+++ Week 05/Event/Event.h
@@ -16,8 +16,8 @@ public:
 	Event(const char* name, const Date& date, const Time& startTime, const Time& endTime);
 	
 	Event(const char* name, unsigned day, unsigned month, unsigned year,
-		  unsigned startTimeHours, unsigned startTimeMins, unsigned startTimeSecs,
-		  unsigned endTimeHours, unsigned endTimeMins, unsigned endTimeSecs);
+	      unsigned startTimeHours, unsigned startTimeMins, unsigned startTimeSecs,
+	      unsigned endTimeHours,   unsigned endTimeMins,   unsigned endTimeSecs);
 	
 	const char* getName() const;
 	const Date& getDate() const;
