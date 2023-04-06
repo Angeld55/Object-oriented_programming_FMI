2e704776bda98883774e237a9ca2cef93af7ccd0
Angel Dimitriev <achi_d@abv.bg>
2023-04-07 01:53:12 +0300

    Add files via upload

---
+++ Week 07/Rational/Rational.cpp
@@ -0,0 +1,108 @@
+#include "Rational.h"
+
+
+
+bool operator==(const Rational& lhs, const Rational& rhs)
+{
+	return lhs.getNom() == rhs.getNom() && lhs.getDenom() == rhs.getDenom();
+}
+
+Rational operator+(const Rational& lhs, const Rational& rhs)
+{
+	Rational lhsCopy(lhs); //copy constr;
+	lhsCopy += rhs;
+	return lhsCopy;
+}
+
+
+Rational::Rational() : Rational(1, 1)
+{}
+Rational::Rational(int nom, int denom)
+{
+	setNom(nom);
+	setDenom(denom);
+}
+
+int Rational::getNom() const
+{
+	return _nom;
+}
+int Rational::getDenom() const
+{
+	return _denom;
+}
+
+void Rational::setNom(int nom)
+{
+	_nom = nom;
+}
+void Rational::setDenom(int denom)
+{
+	if (denom == 0)
+		denom = 0;
+	_denom = denom;
+}
+
+namespace {
+	unsigned getGcd(unsigned a, unsigned b)
+	{
+		if (a < b)
+			std::swap(a, b);
+
+		while (b != 0)
+		{
+			int temp = a % b;
+			a = b;
+			b = temp;
+		}
+		return a;
+	}
+}
+
+
+void Rational::rationalize()
+{
+	unsigned gcd = getGcd(abs(_nom), (abs(_denom)));
+	_nom /= gcd;
+	_denom /= gcd;
+}
+
+Rational& Rational::operator+=(const Rational& other)
+{
+	int oldDenom = _denom;
+	_nom *= other._denom;
+	_denom *= other._denom;
+	int newOtherNom = other._nom * oldDenom;
+	_nom += newOtherNom;
+	return *this;
+}
+Rational& Rational::operator-=(const Rational& other)
+{
+	Rational otherNeg(other);
+	otherNeg._denom *= -1;
+	(*this) += otherNeg;
+	return *this;
+}
+
+Rational& Rational::operator*=(const Rational& other)
+{
+	_nom *= other._nom;
+	_denom *= other._denom;
+	return *this;
+}
+
+Rational& Rational::operator/=(const Rational& other)
+{
+	return (*this) *= Rational(other._denom, other._nom);
+
+}
+
+std::ostream& operator<<(std::ostream& os, const Rational& r)
+{
+	return os << r._nom << " / " << r._denom;
+}
+
+std::istream& operator>>(std::istream& is, Rational& r)
+{
+	return is >> r._nom >> r._denom;
+}
--- Week 07/Rational/Rational.h
+++ Week 07/Rational/Rational.h
@@ -1,1 +1,34 @@
-s
+#pragma once
+#include <fstream>
+
+class Rational
+{
+	int _nom;
+	int _denom;
+
+public:
+	Rational();
+	Rational(int nom, int denom);
+
+	int getNom() const;
+	int getDenom() const;
+
+	void setNom(int nom);
+	void setDenom(int denom);
+
+	void rationalize();
+
+	Rational& operator+=(const Rational& other);
+	Rational& operator-=(const Rational& other);
+	Rational& operator*=(const Rational& other);
+	Rational& operator/=(const Rational& other);
+
+	friend std::ostream& operator<<(std::ostream& os, const Rational& r);
+	friend std::istream& operator>>(std::istream& os, Rational& r);
+
+};
+
+Rational operator+(const Rational& lhs, const Rational& rhs);
+Rational operator-(const Rational& lhs, const Rational& rhs);
+Rational operator/(const Rational& lhs, const Rational& rhs);
+Rational operator*(const Rational& lhs, const Rational& rhs);
