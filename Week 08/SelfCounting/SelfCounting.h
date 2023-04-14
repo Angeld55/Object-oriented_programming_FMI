61cecb27b23c9e3c8348bee4f8311ba537f6f6c9
Angel Dimitriev <angel_dimitriev@abv.bg>
2023-04-14 23:09:12 +0300

    Update SelfCounting.h

--- Week 08/SelfCounting/SelfCounting.h
+++ Week 08/SelfCounting/SelfCounting.h
@@ -3,6 +3,10 @@
 
 class SelfCounting
 {
+		
+	//a constant, which may be different
+	//for different objects
+	//so it is "per object"
 	const int const_val;
 	
 	static unsigned liveObjectsCount;
@@ -14,6 +18,9 @@ public:
 	SelfCounting(const SelfCounting& other);
 	~SelfCounting();
 
+	//methods which aren't
+	//related with particular
+	//instance
 	static unsigned getLiveObjectsCount();
 	static unsigned getCreatedCount();
 
