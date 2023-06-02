1fb860a81adef806dafcbf724db256dc8f718356
Angel Dimitriev <angel_dimitriev@abv.bg>
2023-06-02 10:44:54 +0300

    Update Singleton.h

--- Week 14/SetCalculator/Calculator/Singleton.h
+++ Week 14/SetCalculator/Calculator/Singleton.h
@@ -10,6 +10,6 @@ public:
 	Singleton(char name) : name(name)
 	{}
 
-	virtual bool isElementIn(const Element& el) const override;
-	virtual SetExpression* clone() const override;
+	bool isElementIn(const Element& el) const override;
+	SetExpression* clone() const override;
 };
