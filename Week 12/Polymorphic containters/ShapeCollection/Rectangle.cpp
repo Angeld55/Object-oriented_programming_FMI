b90541ad25bc401c4743790e2a5f1f45af1ca6e4
Plamena Ilieva <92987328+plamena-ilieva@users.noreply.github.com>
2023-06-21 11:43:06 +0300

    Update Rectangle.cpp

--- Week 12/Polymorphic containters/ShapeCollection/Rectangle.cpp
+++ Week 12/Polymorphic containters/ShapeCollection/Rectangle.cpp
@@ -2,6 +2,11 @@
 
 Rectangle::Rectangle(int x1, int y1, int x3, int y3) : Shape(4)
 {
+	if (x1 < x3) 
+		std::swap(x1, x3);
+	if (y1 < y3)
+		std::swap(y1, y3);
+	
 	setPoint(0, x1, y1);
 	setPoint(1, x1, y3);
 	setPoint(2, x3, y3);
@@ -28,7 +33,7 @@ double Rectangle::getPer() const
 bool Rectangle::isPointIn(int x, int y) const
 {
 	Shape::point p(x, y);
-	return p.x >= getPointAtIndex(0).x && p.y >= getPointAtIndex(1).x &&
+	return p.x >= getPointAtIndex(0).x && p.x <= getPointAtIndex(2).x &&
 		p.y <= getPointAtIndex(0).y && p.y >= getPointAtIndex(2).y;
 }
 Shape* Rectangle::clone() const
