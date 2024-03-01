4c823036ecab5700ea4afdd831180de5a0dc08d5
Desislava Shunina <115353472+desiish@users.noreply.github.com>
2024-03-01 13:54:51 +0200

    remove struct examples from pract01 snippets

--- Utils/Pract_si_3/Pract01/snippets.cpp
+++ Utils/Pract_si_3/Pract01/snippets.cpp
@@ -1,48 +1,4 @@
 #include <iostream>
-// Structs
-struct Empty {
-
-};
-
-struct Test1 {
-    int a;
-};
-
-struct Test2 {
-    char* a;
-    int* b;
-};
-
-struct Test3 {
-    char a[3];
-    short b;
-    double c[2];
-    char d;
-};
-
-struct Test4 {
-    int a[3];
-    short b;
-    char c[];
-};
-
-struct A {
-    int i;
-    bool b;
-    double d;
-};
-
-struct B {
-    A obj;
-    int c;
-};
-
-struct C {
-    A objA;
-    B objB;
-};
-
-
 //Initialization of enum class
 
 namespace day {
