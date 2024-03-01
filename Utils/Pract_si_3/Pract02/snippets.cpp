b9af7d9683b17d13788bd1c6a175e5e90f4d37fc
Desislava Shunina <115353472+desiish@users.noreply.github.com>
2024-03-01 15:26:14 +0200

    Update snippets.cpp

--- Utils/Pract_si_3/Pract02/snippets.cpp
+++ Utils/Pract_si_3/Pract02/snippets.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 // Structs
 
 struct Empty {
@@ -53,5 +54,36 @@ union {
 int main() {
     unionExample.n = 257;
     unionExample.arr[0] = 'a'; // 'a' = 97
-    std::cout << unionExample.n;
+    std::cout << unionExample.n << '\n';
+//eofbit
+    {
+        std::ifstream ifs("numbers.txt");
+        if (!ifs.is_open())
+            std::cout << "The file is not opened." << '\n';
+        else
+            std::cout << "The file was opened successfully." << '\n';
+        while (true)
+        {
+            int n;
+            ifs >> n;
+            if (ifs.eof())
+                break;
+        }
+        std::cout << "Badbit:  " << ifs.bad() << "\nFailbit: " << ifs.fail() << "\nEofbit: " << ifs.eof() << "\nGoodbit: " << ifs.good() << '\n';
+        ifs.clear();
+        std::cout << "After ifs.clear():\n";
+        std::cout << "Badbit:  " << ifs.bad() << "\nFailbit: " << ifs.fail() << "\nEofbit: " << ifs.eof() << "\nGoodbit: " << ifs.good() << '\n';
+    }
+//failbit
+    {
+        std::ifstream ifs2("notANumber.txt");
+        if (!ifs2.is_open())
+            std::cout << "The file is not opened." << '\n';
+        else
+            std::cout << "The file was opened successfully." << '\n';
+
+        int num;
+        ifs2 >> num;
+        std::cout << "Badbit:  " << ifs2.bad() << "\nFailbit: " << ifs2.fail() << "\nEofbit: " << ifs2.eof() << "\nGoodbit: " << ifs2.good() << '\n';
+    }
 }
