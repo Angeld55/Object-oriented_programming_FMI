8726ff949d3b54fb70b13bb973afaae93d9b07f2
Angel Dimitriev <angel_dimitriev@abv.bg>
2024-03-03 01:39:08 +0200

    Update 4. struct_from_to_file.cpp

--- Week 02/4. struct_from_to_file.cpp
+++ Week 02/4. struct_from_to_file.cpp
@@ -11,19 +11,6 @@ enum class Major
     S
 };
 
-const char* getMajorStr(Major m)
-{
-    switch (m)
-    {
-    case Major::Inf: return "Inf";
-    case Major::Cs: return "Cs";
-    case Major::InfS: return "InfS";
-    case Major::S: return "S";
-
-    }
-    return "";
-}
-
 struct Student
 {
     char name[100] = "";
@@ -31,10 +18,7 @@ struct Student
     Major major = Major::undefined;
 };
 
-void printStudent(const Student& st)
-{
-    std::cout << st.name << " " << st.fn << " " << getMajorStr(st.major) << std::endl;
-}
+
 
 const int STUDENTS_MAX_SIZE = 10;
 struct StudentDatabase
@@ -43,60 +27,18 @@ struct StudentDatabase
     Student students[STUDENTS_MAX_SIZE];
 };
 
-
-void printDatabase(const StudentDatabase& database)
-{
-    assert(database.studentsCount <= STUDENTS_MAX_SIZE);
-
-    for (int i = 0; i < database.studentsCount; i++)
-        printStudent(database.students[i]);
-}
-
-enum class Criteria
-{
-    sortByName,
-    sortByMajor,
-    sortByFn
-};
-
-void sortDatabase(StudentDatabase& database, bool(*isLess)(const Student& lhs, const Student& rhs))
-{
-    for (int i = 0; i < database.studentsCount; i++)
-    {
-        int minIndex = i;
-        for (int j = i + 1; j < database.studentsCount; j++)
-        {
-            if (isLess(database.students[j], database.students[minIndex]))
-                minIndex = j;
-
-        }
-        if (i != minIndex)
-            std::swap(database.students[i], database.students[minIndex]);
-    }
-}
-
-void sortByCriteria(StudentDatabase& database, Criteria cr)
-{
-    switch (cr)
-    {
-    case Criteria::sortByName: return sortDatabase(database, [](const Student& lhs, const Student& rhs) { return strcmp(lhs.name, rhs.name) < 0; });
-    case Criteria::sortByMajor: return sortDatabase(database, [](const Student& lhs, const Student& rhs) { return lhs.major < rhs.major; });
-    case Criteria::sortByFn: return sortDatabase(database, [](const Student& lhs, const Student& rhs) { return lhs.fn < rhs.fn; });
-    }
-}
-
 void serializeStudent(std::ostream& os, const Student& st)
 {
     os << st.name << std::endl << st.fn << std::endl << (int)st.major << std::endl;
 }
-void serialize(std::ostream& os, const StudentDatabase& database)
+void serializeDatabase(std::ostream& os, const StudentDatabase& database)
 {
     os << database.studentsCount << std::endl;
     for (int i = 0; i < database.studentsCount; i++)
         serializeStudent(os, database.students[i]);
 }
 
-void deserialize(std::istream& ifs, Student& st)
+void deserializeStudent(std::istream& ifs, Student& st)
 {
     ifs.ignore(); //there was a number on row before the string!!!!
     ifs.getline(st.name, 100);
@@ -105,12 +47,12 @@ void deserialize(std::istream& ifs, Student& st)
     ifs >> tempMajor;
     st.major = (Major)tempMajor;
 }
-StudentDatabase deserialize(std::istream& ifs)
+StudentDatabase deserializeDatabase(std::istream& ifs)
 {
     StudentDatabase st;
     ifs >> st.studentsCount;
     for (int i = 0; i < st.studentsCount; i++)
-        deserialize(ifs, st.students[i]);
+        deserializeStudent(ifs, st.students[i]);
     return st;
 }
 int main()
@@ -127,7 +69,7 @@ int main()
             "Petur Aleksandrov", 3312, Major::S,
             "Kiril Petkov", 3312, Major::S
         };
-        serialize(ofs, db);
+        serializeDatabase(ofs, db);
     }
     {
         std::ifstream ifs("students_to_text_file.txt");
@@ -137,7 +79,7 @@ int main()
             return -1;
         }
 
-        StudentDatabase db = deserialize(ifs);
-        serialize(std::cout, db); //print to the console
+        StudentDatabase db = deserializeDatabase(ifs);
+        serializeDatabase(std::cout, db); //print to the console
     }
 }
