8b8929f56528f92b44db62ca97d5debcda77d9f2
Angel Dimitriev <angel_dimitriev@abv.bg>
2024-04-03 12:17:58 +0300

    Create NVector.h

---
+++ Week 07/NVector/NVector.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <iostream>
+
+class Nvector
+{
+private:
+	int* data;
+	size_t size;
+
+	void copyFrom(const Nvector& other);
+	void free();
+
+public:
+	Nvector(size_t size);
+
+	Nvector(const Nvector& other);
+	Nvector& operator=(const Nvector& other);
+	~Nvector();
+
+
+	Nvector& operator+=(const Nvector& other);
+	Nvector& operator-=(const Nvector& other);
+
+
+	Nvector& operator*=(size_t scalar);
+
+	int& operator[](size_t); //get set
+	int operator[](size_t) const; // get
+
+
+	size_t operator~() const;// size;
+
+	friend std::ostream& operator<<(std::ostream& os, const Nvector& v);
+	friend std::istream& operator>>(std::istream&, Nvector& v);
+};
+
+Nvector operator+(const Nvector& lhs, const Nvector& rhs);
+Nvector operator-(const Nvector& lhs, const Nvector& rhs);
+
+
+//So it could be commutative:
+Nvector operator*(const Nvector& lhs, size_t scalar);
+Nvector operator*(size_t scalar, const Nvector& lhs);
+
+
+//are parallel
+bool operator||(const Nvector& lhs, const Nvector& rhs);
+
+
+//scalar product
+size_t operator%(const Nvector& lhs, const Nvector& rhs);
+
+
+//are perpendicular
+bool operator|=(const Nvector& lhs, const Nvector& rhs);
