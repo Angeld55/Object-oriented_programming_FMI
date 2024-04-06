6da5234f8f833e9b7e52350205cbbb33f88384d0
Aneliya21 <115350258+Aneliya21@users.noreply.github.com>
2024-04-06 16:00:26 +0300

    Update 1. newLinesCount.cpp

--- Week 02/1. newLinesCount.cpp
+++ Week 02/1. newLinesCount.cpp
@@ -2,9 +2,42 @@
 #include <fstream>
 #include <assert.h>
 
+const char NEW_LINE_CHAR = '\n';
+
+int getCharCount(std::ifstream& ifs, char ch) {
+
+	if (!ifs.is_open()) {
+		return -1;
+	}
+
+	int count = 0;
+	while (true) {
+		char current = ifs.get();
+
+		if (ifs.eof()) {
+			return count + 1;
+		}
+
+		if (current == ch) {
+			count++;
+		}
+	}
+	return 0;
+}
+
+int getLinesCount(const char* fileName) {
+	if (!fileName) {
+		return -1;
+	}
+
+	std::ifstream ifs(fileName);
+	if (!ifs.is_open()) {
+		return -1;
+	}
+
+	return getCharCount(ifs, NEW_LINE_CHAR);
+}
 int main()
 {
-	std::ofsteam ofs("test.txt");
-
-	ofs << 1;
+	std::cout << getLinesCount("file.txt");
 }
