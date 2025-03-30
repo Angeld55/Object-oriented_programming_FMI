914459b5aa1b94775e4bc78e1a79ef3e1d0b0207
Angel Dimitriev <angel_dimitriev@abv.bg>
2025-03-30 16:39:47 +0300

    Update BulgarianDate.h

--- Week 05/Date, Time, Event/Date/BulgarianDate.h
+++ Week 05/Date, Time, Event/Date/BulgarianDate.h
@@ -1,40 +1,62 @@
 #pragma once
 
+enum class Month
+{
+    January = 1,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+};
+
+enum class DayOfWeek
+{
+    Monday = 1,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+};
+
 class BulgarianDate
 {
-	unsigned MAX_DAYS[12] =
-	{
-		31,28,31,30,31, 30, 31, 31, 30, 31, 30, 31
-	};
-	unsigned day = 1;
-	unsigned month = 1;
-	unsigned year = 1;
-
-	bool isLeapYear() const;
-        void validateDate();
+    unsigned MAX_DAYS[12] =
+    {
+        31,28,31,30,31,30,31,31,30,31,30,31
+    };
+    unsigned day = 1;
+    Month month = Month::January;
+    unsigned year = 1;
     
-        bool isDateInValidState = true;
-	mutable bool isModified = true;
-	mutable int dayOfWeek = -1;
+    bool isLeapYear() const;
+    void validateDate();
+    bool isDateInValidState = true;
+    mutable bool isModified = true; //member that can be modified from CONST functions!! 
+    mutable int dayOfWeek = -1;
 
 public:
- 	BulgarianDate() = default;
-	BulgarianDate(unsigned day, unsigned month, unsigned year);
-	unsigned getDay() const;
-	unsigned getMonth() const;
-	unsigned getYear() const;
-
-	void setDay(unsigned day);
-	void setMonth(unsigned month);
-	void setYear(unsigned year);
-
-	void print() const;
-	void goToNextDay();
-	int getDayOfWeek() const;
-	
-	bool good() const;
-	void clear();
-
+    BulgarianDate();
+    BulgarianDate(unsigned day, unsigned month, unsigned year);
+    unsigned getDay() const;
+    unsigned getMonth() const;
+    unsigned getYear() const;
+    void setDay(unsigned day);
+    void setMonth(unsigned month);
+    void setYear(unsigned year);
+    void print() const;
+    void goToNextDay();
+    DayOfWeek getDayOfWeek() const;
+    bool good() const;
+    void clear();
 };
 
 int compareBulgarianDates(const BulgarianDate& lhs, const BulgarianDate& rhs);
