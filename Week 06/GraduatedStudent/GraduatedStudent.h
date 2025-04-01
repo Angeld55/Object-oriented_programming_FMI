62cce24810b6894169420b8ae2a1184963dcfdc5
Angel Dimitriev <angel_dimitriev@abv.bg>
2025-04-01 13:21:47 +0300

    Update GraduatedStudent.h

--- Week 06/GraduatedStudent/GraduatedStudent.h
+++ Week 06/GraduatedStudent/GraduatedStudent.h
@@ -1,31 +1,26 @@
 #pragma once
 class GraduatedStudent
 {
-	char* name = nullptr;
-	int* grades = nullptr;
-	size_t gradesCount = 0;
-	char quote[30 + 1] = "";
-
-	void copyFrom(const GraduatedStudent& other);
-	void free();
-
 public:
-    GraduatedStudent(const char* name,
-                     const int* grades,
-                     size_t gradesCount,
-                     const char* quote);
+    GraduatedStudent(const char* name, const int* grades, size_t gradesCount, const char* quote);
+    GraduatedStudent(const GraduatedStudent& other);
+    GraduatedStudent& operator=(const GraduatedStudent& other);
+    ~GraduatedStudent();
 
-	GraduatedStudent(const GraduatedStudent& other);
-	GraduatedStudent& operator=(const GraduatedStudent& other);
-	~GraduatedStudent();
+    void setName(const char* newName);
+    void setGrades(const int* newGrades, size_t newGradesCount);
+    void setQuote(const char* quote);
 
+    const char* getName() const;
+    const int* getGrades() const;
+    unsigned getGradesCount() const;
+    const char* getQuote() const;
 
-	void setName(const char* newName);
-	void setGrades(const int* newGrades, size_t newGradesCount);
-	void setQuote(const char* quote);
-
-	const char* getName() const;
-	const int* getGrades() const;
-	unsigned getGradesCount() const;
-	const char* getQuote() const;
+private:
+    char* name = nullptr;
+    int* grades = nullptr;
+    size_t gradesCount = 0;
+    char quote[31] = "";
+    void copyDynamic(const GraduatedStudent& other);
+    void freeDynamic();
 };
