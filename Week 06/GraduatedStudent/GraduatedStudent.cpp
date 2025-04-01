b754ef0631e35aaa21d71d49e6d8428ef62c7087
Angel Dimitriev <angel_dimitriev@abv.bg>
2025-04-01 13:22:00 +0300

    Update GraduatedStudent.cpp

--- Week 06/GraduatedStudent/GraduatedStudent.cpp
+++ Week 06/GraduatedStudent/GraduatedStudent.cpp
@@ -1,106 +1,100 @@
 #include "GraduatedStudent.h"
 #include <cstring>
-#pragma warning (disable : 4996)
+#pragma warning(disable: 4996)
 
-//we assume that the object is "empty". No need for delete
-void GraduatedStudent::copyFrom(const GraduatedStudent& other)
+void GraduatedStudent::copyDynamic(const GraduatedStudent& other)
 {
-	name = new char[strlen(other.name) + 1];
-	strcpy(name, other.name);
-
-	grades = new int[other.gradesCount];
-	for (int i = 0; i < other.gradesCount; i++)
-		grades[i] = other.grades[i];
-	gradesCount = other.gradesCount;
-
-	strcpy(quote, other.quote);
-}
-void GraduatedStudent::free()
-{
-	delete[] name;
-	delete[] grades;
-
-	name = nullptr;
-	grades = nullptr;
-	gradesCount = 0;
-	strcpy(quote, "");
+    name = new char[strlen(other.name) + 1];
+    strcpy(name, other.name);
+    grades = new int[other.gradesCount];
+    for (int i = 0; i < other.gradesCount; i++)
+        grades[i] = other.grades[i];
 }
 
-
-GraduatedStudent::GraduatedStudent(const char* name,
-	const int* grades,
-	size_t gradesCount,
-	const char* quote)
+void GraduatedStudent::freeDynamic()
 {
-	setName(name);
-	setGrades(grades, gradesCount);
-	setQuote(quote);
+    delete[] name;
+    delete[] grades;
+    name = nullptr;
+    grades = nullptr;
+    gradesCount = 0;
+    strcpy(quote, "");
 }
+
+GraduatedStudent::GraduatedStudent(const char* name, const int* grades, size_t gradesCount, const char* quote)
+{
+    setName(name);
+    setGrades(grades, gradesCount);
+    setQuote(quote);
+}
+
 GraduatedStudent::GraduatedStudent(const GraduatedStudent& other)
+    : gradesCount(other.gradesCount)
 {
-	copyFrom(other);
+    copyDynamic(other);
+    strcpy(quote, other.quote);
 }
 
 GraduatedStudent& GraduatedStudent::operator=(const GraduatedStudent& other)
 {
-	if (this != &other)
-	{
-		free();
-		copyFrom(other);
-	}
-	return *this;
+    if (this != &other)
+    {
+        freeDynamic();
+        gradesCount = other.gradesCount;
+        strcpy(quote, other.quote);
+        copyDynamic(other);
+    }
+    return *this;
 }
-
 
 GraduatedStudent::~GraduatedStudent()
 {
-	free();
+    freeDynamic();
 }
-
 
 void GraduatedStudent::setName(const char* newName)
 {
-	if (!newName || newName == name) //setName(getName()) !!!!
-		return;
-	delete[] name;
-	name = new char[strlen(newName) + 1];
-	strcpy(name, newName);
+    if (!newName || newName == name)
+        return;
+    delete[] name;
+    name = new char[strlen(newName) + 1];
+    strcpy(name, newName);
 }
+
 void GraduatedStudent::setGrades(const int* newGrades, size_t newGradesCount)
 {
-	if (!newGrades || grades == newGrades)
-		return;
-
-	delete[] grades;
-	grades = new int[newGradesCount];
-	gradesCount = newGradesCount;
-	for (int i = 0; i < newGradesCount; i++)
-		grades[i] = newGrades[i];
+    if (!newGrades || grades == newGrades)
+        return;
+    delete[] grades;
+    grades = new int[newGradesCount];
+    gradesCount = newGradesCount;
+    for (int i = 0; i < newGradesCount; i++)
+        grades[i] = newGrades[i];
 }
-
 
 void GraduatedStudent::setQuote(const char* newQuote)
 {
-	if (!newQuote || strlen(newQuote) > 30)
-		return;
-	strcpy(quote, newQuote);
+    if (!newQuote || strlen(newQuote) > 30)
+        return;
+    strcpy(quote, newQuote);
 }
 
 const char* GraduatedStudent::getName() const
 {
-	return name;
+    return name;
 }
+
 const int* GraduatedStudent::getGrades() const
 {
-	return grades;
+    return grades;
 }
 
 unsigned GraduatedStudent::getGradesCount() const
 {
-	return gradesCount;
+    return gradesCount;
 }
 
 const char* GraduatedStudent::getQuote() const
 {
-	return quote;
+    return quote;
 }
